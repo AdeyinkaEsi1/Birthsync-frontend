1a251b40bbbf390a15070d9be36ccf3ca52f83d8
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-06-20 22:50:12 +0100

    upd

--- main.js
+++ main.js
@@ -7,21 +7,28 @@ form.addEventListener('submit', async (event) => {
     const formData = new FormData(form);
     const data = {
         name: formData.get('name'),
-        birthdate: formData.get('birthdate'),
-        optional_info: formData.get('info') || null,
+        birth_date: formData.get('birth_date'),
+        extra_info: formData.get('extra_info') || null,
     };
 
+
+    console.log('Form Data:', data); // Log the data object
+
+    const new_data = JSON.stringify(data);
+    console.log('JSON Data:', new_data);
     // Send data to the backend using Axios
     try {
-        const response = await axios.post('http://127.0.0.1:8000/bday/new', JSON.stringify(data));
+        const response = await axios.post('http://127.0.0.1:8000/bday/new', new_data, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
         // Display success message
+        console.log(response);
         alert(response.data.message);
     } catch (error) {
-        if (error.response){
-        // Display error message
-        alert('Error submitting data: ' + error.response.data.detail);
-    } else {
-        alert('Error submitting data: ' + error.message)
-    }}
+        alert('Error submitting data: ' + (error.response?.data?.message || error.message));
+    }
 });
 
+// Error submitting data1: Cannot read properties of undefined (reading 'message')
